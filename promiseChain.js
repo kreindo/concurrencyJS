854806cfe6aeabf9e3d18348559a7e232dbf65f7
ahmadsan <38377576+kreindo@users.noreply.github.com>
2022-07-16 02:38:41 +0000

    added Promise.all

--- promiseChain.js
+++ promiseChain.js
@@ -1,37 +1,63 @@
 const fab = {
-  isFunctional : true,
-  reqMaint : false
+  isFunctional: true,
+  reqMaint: false
 }
 
 const stock = {
-  skin : null,
-  innards : 9999
+  skin: 10000,
+  innards: 9999
 }
 
 
-checkFabr = ({isFunctional, reqMaint}) => {
+checkFabr = ({ isFunctional, reqMaint }) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-    if(isFunctional && !reqMaint) {
-      resolve('Fabricator ready')
-    } else {
-      reject('requirements not met') 
-    }}, 5000)
+      if (isFunctional && !reqMaint) {
+        resolve('Fabricator ready')
+      } else {
+        reject('requirements not met')
+      }
+    }, 5000)
   })
 }
 
+const installWires = () => {
+  return new Promies((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Wire installed')
+
+    }, 5000)
+  })
+}
+
+const downloadAI = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('AI downloaded')
+    }, 4000)
+  })
+}
+
+const installAI = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('AI Installed')
+
+    }, 5000)
+  })
+}
 const fabInit = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(()=>{
+    setTimeout(() => {
       resolve('creature made')
     }, 20000)
   })
 }
 
-const checkStock = ({skin, innards}) => {
+const checkStock = ({ skin, innards }) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if(skin >= 100 && innards >= 100) {
+      if (skin >= 100 && innards >= 100) {
         resolve('ingredients met')
       } else {
         reject('req not met')
@@ -42,20 +68,26 @@ const checkStock = ({skin, innards}) => {
 
 function makeCreature(fabState, para) {
   checkFabr(fabState)
-  .then((value) => {
-    console.log(value)
-    return checkStock(stock);
-  })
-  .then((value) => {
-    console.log(value)
-    return fabInit();
-  })
-  .then((value) => {
-    console.log(value)
-  })
-  .catch(rejectedReason => {
-    console.log(rejectedReason)
-  })
+    .then((value) => {
+      console.log(value)
+      return checkStock(stock);
+    })
+    .then((value) => {
+      console.log(value)
+      const promises = [installWires(), downloadAI(), installAI()]
+
+      return Promise.all(promises)
+    })
+    .then((value) => {
+      console.log(value)
+      return fabInit();
+    })
+    .then((value) => {
+      console.log(value)
+    })
+    .catch(rejectedReason => {
+      console.log(rejectedReason)
+    })
 }
 
 makeCreature(fab)
